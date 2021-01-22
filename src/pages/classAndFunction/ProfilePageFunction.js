386b72c928c5acc913c10a2303fd19e379dc8927
qiulijuan <qiulijuan@beyondcent.com>
2021-01-22 08:43:21 +0800

    useEffect, useRef获取previous state；函数组件和类组件的差异

--- src/pages/classAndFunction/ProfilePageFunction.js
+++ src/pages/classAndFunction/ProfilePageFunction.js
@@ -4,11 +4,9 @@ function ProfilePage(props) {
   const showMessage = () => {
     alert('Followed ' + props.user);
   };
-
   const handleClick = () => {
     setTimeout(showMessage, 3000);
   };
-
   return <button onClick={handleClick}>Follow</button>;
 }
 
