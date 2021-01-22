9f2d7a3416860a77dda54fe2e8fa5bbf545dbb71
qiulijuan <qiulijuan@beyondcent.com>
2021-01-22 14:33:01 +0800

    previous state

--- src/pages/useRefDemo.js
+++ src/pages/useRefDemo.js
@@ -4,22 +4,22 @@ const Profile = () => {
   console.log(0);
   const [user, setUser] = React.useState({ name: 'Alex', weight: 40 });
   const prevUser = usePrevious(user);
+  // console.log(user)
+  // console.log(prevUser)
   React.useEffect(() => {
-    console.log(5) &&
-      prevUser &&
-      user.weight > prevUser.weight &&
-      console.log('You need to do exercise!');
+    console.log(5);
+    prevUser && user.weight > prevUser.weight && console.log('你需要减肥啦！');
   }, [user]);
   console.log(3);
   const gainWeight = () => {
-    const newWeight = Math.random() >= 0.5 ? user.weight : user.weight + 1;
+    const newWeight = Math.random() <= 0.5 ? user.weight : user.weight + 1;
     setUser(user => ({ ...user, weight: newWeight }));
   };
 
   return (
     <>
-      <p>Current weight: {user.weight}</p>
-      <button onClick={gainWeight}>Eat burger</button>
+      <p>当前体重: {user.weight}</p>
+      <button onClick={gainWeight}>吃饭</button>
     </>
   );
 };
